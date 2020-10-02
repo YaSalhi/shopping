b8ad1914f308ca38f767185c9ede705ab50b9545
YaSalhi <sayassine54@gmail.com>
2020-10-02 20:21:02 +0100

    initial commit

---
+++ database/factories/ModelscountryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Modelscountry;
+use Faker\Generator as Faker;
+
+$factory->define(Modelscountry::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
